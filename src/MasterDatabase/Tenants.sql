fe248a51b378e4d9a40032a7953d8e2f6ccdb85c
Remco Brosky <remcobrosky@microsoft.com>
2022-04-29 14:54:50 +0200

    DataModel for DataRouter added

---
+++ src/MasterDatabase/Tenants.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Tenants]
+(
+	[tenant_id] UNIQUEIDENTIFIER NOT NULL PRIMARY KEY DEFAULT newid(), 
+    [tenant_aadtentantid] NVARCHAR(50) NOT NULL,
+    [tenant_name] NVARCHAR(50) NOT NULL, 
+    [tenant_dbserver] NVARCHAR(50) NOT NULL, 
+    [tenant_dbname] NVARCHAR(50) NOT NULL
+)
